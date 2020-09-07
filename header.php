57247d27ef957d148decddd23bbe01790837e91e
IngJirayu <70875926+IngJirayu@users.noreply.github.com>
2020-09-07 12:08:52 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Bench</title>
+</head>
+<body>
+<nav class="header">
+    <a href="#" class="logo">SUPERSUBS</a>
+    <input class="menu-btn" type="checkbox" id="menu-btn"/>
+    <label class="menu-icon" for="menu-btn"><span class="nav-icon"></span></label>
+    <ul class="menu">
+      <li><a href="index.php">หน้าแรก</a></li>
+      <li><a href="#">แท๊ก</a></li>
+      <li><a href="#">สร้างบทสนทนา</a></li>
+      <li><a href="login.php">ลงชื่อเข้าใช้</a></li>
+    </ul>
+</nav>
